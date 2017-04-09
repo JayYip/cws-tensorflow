Add a softmax layer, fix vocab

data/build_pku_msr_input.py
@@ -60,7 +60,7 @@ FLAGS = tf.flags.FLAGS
 class Vocabulary(object):
   """Simple vocabulary wrapper."""
 
-  def __init__(self, vocab, unk_id, unk_word = '<UNK>'):
+  def __init__(self, vocab, id_vocab, unk_id, unk_word = '<UNK>'):
     """Initializes the vocabulary.
 
     Args:
@@ -68,8 +68,10 @@ class Vocabulary(object):
       unk_id: Id of the special 'unknown' word.
     """
     self._vocab = vocab
+    self._id_vocab = id_vocab
     self._unk_id = unk_id
-    self._vocab[unk_word] = 0
+    self._vocab[unk_word] = len(self._vocab)
+    self._id_vocab[len(self._vocab)] = unk_word
 
   def word_to_id(self, word):
     """Returns the integer id of a word string."""
@@ -81,9 +83,9 @@ class Vocabulary(object):
   def id_to_word(self, word_id):
     """Returns the word string of an integer word id."""
     if word_id >= len(self._vocab):
-      return self._vocab[self.unk_id]
+      return self._id_vocab[self.unk_id]
     else:
-      return self._vocab[word_id]
+      return self._id_vocab[word_id]
 
 def tag_to_id(t):
     if t == 's':
@@ -234,7 +236,8 @@ def _create_vocab(path_list):
     reverse_vocab = [x[0] for x in word_counts]
     unk_id = len(reverse_vocab)
     vocab_dict = dict([(x, y) for (y, x) in enumerate(reverse_vocab)])
-    vocab = Vocabulary(vocab_dict, unk_id)
+    id_vocab_dict = dict([(y, x) for (y, x) in enumerate(reverse_vocab)])
+    vocab = Vocabulary(vocab_dict, id_vocab_dict, unk_id)
 
     return vocab
 
@@ -383,7 +386,7 @@ def main(unused_argv):
         # Windows may complain if the folders already exist
         pass
 
-    path_list = download_extract(FLAGS.data_source, 'Y')
+    path_list = download_extract(FLAGS.data_source, 'N')
 
     vocab = _create_vocab(path_list)
     pickle.dump(vocab, open('vocab.pkl', 'wb'))
@@ -399,7 +402,7 @@ def main(unused_argv):
 
     path_list = trimmed_path_list
 
-    _process_dataset('train', path_list, vocab)
+    #_process_dataset('train', path_list, vocab)
 
 
 if __name__ == '__main__':

inference.py
@@ -22,7 +22,7 @@ from lstm_based_cws_model import LSTMCWS
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.flags.DEFINE_string("input_file_dir", "data\\output_dir\\icwb2-data\\testing\\",
+tf.flags.DEFINE_string("input_file_dir", "data/output_dir/icwb2-data/testing/",
                        "Path of input files.")
 tf.flags.DEFINE_string("vocab_dir", "data/vocab.pkl",
                        "Path of vocabulary file.")
@@ -104,7 +104,6 @@ def main(unused_argv):
             if fullpath.split('.')[-1] in ['utf8', 'txt', 'csv']:
                 filename_list.append(fullpath)
 
-    #filename_list = ['data\\output_dir\\icwb2-data\\testing\\as_test.utf8']
     checkpoint_path = FLAGS.train_dir
 
     model_config = configuration.ModelConfig()
@@ -120,8 +119,9 @@ def main(unused_argv):
 
         #Add transition var to graph
         with tf.variable_scope('tag_inf') as scope:
-            transition_param = tf.get_variable(name = 'transition_param', 
-                shape = [model_config.num_tag,model_config.num_tag])
+            transition_param = tf.Variable(name = 'transitions', 
+                initial_value = 0,
+                validate_shape=False)
 
         #Build model
         model = LSTMCWS(model_config, 'inference')

lstm_based_cws_model.py
@@ -18,6 +18,18 @@ import tensorflow as tf
 import os
 from ops import input_ops
 
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+          stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
 
 class LSTMCWS(object):
     """docstring for LSTMCWS"""
@@ -127,7 +139,7 @@ class LSTMCWS(object):
         with tf.variable_scope('seq_embedding', reuse = True) as seq_embedding_scope:
             #chr_embedding = tf.Variable(self.embedding_tensor, name="chr_embedding")
             if self.is_training():
-                chr_embedding = tf.get_variable(name="chr_embedding", validate_shape = False)
+                chr_embedding = tf.get_variable(name="chr_embedding", validate_shape = False, trainable=False)
             else:
                 chr_embedding = tf.Variable(tf.zeros([10]), validate_shape=False, name="chr_embedding")
 
@@ -198,23 +210,25 @@ class LSTMCWS(object):
                 weights_initializer = self.initializer,
                 scope = logit_scope)
 
+        softmax = tf.nn.softmax(logits = logit)
+
         if not self.is_training():
             # In inference, logit will be returned since the decode fn only takes numpy array.
-            logit = tf.squeeze(logit)
+            softmax = tf.squeeze(softmax)
 
-            self.logit = logit
+            self.logit = softmax
 
         else:
             with tf.variable_scope('tag_inf') as tag_scope:
                 sequence_length = tf.reduce_sum(self.input_mask, 1)
-                sentence_likelihood, transition_param = tf.contrib.crf.crf_log_likelihood(inputs = logit,
+                sentence_likelihood, transition_param = tf.contrib.crf.crf_log_likelihood(inputs = softmax,
                     tag_indices = tf.to_int32(self.tag_seqs),
                     sequence_lengths = sequence_length)
 
-                share_transition_param = tf.get_variable(name = 'transition_param', 
-                    shape = [self.config.num_tag,self.config.num_tag],
-                    initializer = self.initializer)
-                ass_op = share_transition_param.assign(transition_param)
+                #share_transition_param = tf.get_variable(name = 'transition_param', 
+                #    shape = [self.config.num_tag,self.config.num_tag],
+                #    initializer = self.initializer)
+                #ass_op = share_transition_param.assign(transition_param)
 
             batch_loss = tf.reduce_sum(-sentence_likelihood)
 

train.py
@@ -56,7 +56,8 @@ def main(unused_argv):
         #Set embedding table
         embedding = tf.convert_to_tensor(chr_embedding, dtype = tf.float32)
         with tf.variable_scope('seq_embedding') as seq_embedding_scope:
-            chr_embedding_var = tf.get_variable(name = 'chr_embedding', shape = (shape[0], shape[1]))
+            chr_embedding_var = tf.get_variable(name = 'chr_embedding', 
+                shape = (shape[0], shape[1]), trainable=False)
             embedding_assign_op = chr_embedding_var.assign(embedding)
 
         #Build model
@@ -103,7 +104,7 @@ def main(unused_argv):
     tf.contrib.slim.learning.train(
         train_op,
         train_dir,
-        log_every_n_steps=FLAGS.log_every_n_steps,
+        log_every_n_steps=1,
         graph=g,
         global_step=model.global_step,
         number_of_steps=train_config.training_step,